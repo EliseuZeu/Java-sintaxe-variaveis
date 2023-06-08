ea7030f11793091c6546dab42d1b0bc16b7c2a8e
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-08 17:34:11 -0300

    println

--- sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -7,6 +7,7 @@ public class TestaConversao {
         long numeroGrande = 1231345325353l;
         short numeroPequeno = 13;
         float pontoFlutuante = 3.14f;
-
+        System.out.println("Numero grande e" + numeroGrande + ", Numero pequeno e " + numeroPequeno
+                + "e ponto Flutuante e" + pontoFlutuante);
     }
 }
