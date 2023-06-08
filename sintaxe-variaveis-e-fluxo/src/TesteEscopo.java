e998a496ff58dadc0a74c4e6975c761597b8f9e6
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-08 20:58:18 -0300

    condicional teste

---
+++ sintaxe-variaveis-e-fluxo/src/TesteEscopo.java
@@ -0,0 +1,16 @@
+public class TesteEscopo {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        int quantidadePessoas = 3;
+
+        boolean acompanhado = true;
+
+        if (idade >= 18 && acompanhado) {
+            System.out.println("Voce tem mais de 18 anos");
+            System.out.println("Bem vindo");
+        } else {
+            System.out.println("Voce e menor de idade");
+        }
+    }
+}
