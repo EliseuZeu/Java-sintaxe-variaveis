4878287bb922eda2ec74d1ad8def304f8e658df7
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-08 16:47:43 -0300

    apredendo variaveis com java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFluatuante.java
@@ -0,0 +1,5 @@
+public class TestaPontoFluatuante {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,18 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Hello, novo teste!");
+
+        int idade;
+        idade = 37;
+
+        System.out.println(idade);
+
+        idade = 30 + 10;
+
+        System.out.println(idade);
+
+        idade = (30 - 10) + 2;
+
+        System.out.println("Sua idade e " + idade + ", Parabens");
+    }
+}
