11e93e47a90ab823768ce62c8c2fb00e0ea2141a
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-08 16:53:36 -0300

    Teste com tipo double e int resolvendo diferença

--- sintaxe-variaveis-e-fluxo/src/TestaPontoFluatuante.java
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFluatuante.java
@@ -1,5 +1,18 @@
+
 public class TestaPontoFluatuante {
     public static void main(String[] args) {
 
+        double salario;
+        salario = 1250.70;
+        System.out.println("Meu salario e " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+
+        double novaTentativa = 5.0 / 2;
+        System.out.println(novaTentativa);
     }
 }
