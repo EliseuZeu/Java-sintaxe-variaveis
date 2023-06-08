3573905e5ccdf58392fd10c6a6110e9358058d9f
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-08 17:02:38 -0300

    numneros flutuante diferenças tipo numericos

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,12 @@
+public class TestaConversao {
+    public static void main(String[] args) {
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+
+        long numeroGrande = 1231345325353l;
+        short numeroPequeno = 13;
+        float pontoFlutuante = 3.14f;
+
+    }
+}
