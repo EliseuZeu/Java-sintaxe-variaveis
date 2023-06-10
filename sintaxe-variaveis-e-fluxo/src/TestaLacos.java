5171efbb6ef101b6df3401fc652ae3dd4aeeb92a
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-10 12:55:49 -0300

    Tabuado usando laços

---
+++ sintaxe-variaveis-e-fluxo/src/TestaLacos.java
@@ -0,0 +1,9 @@
+public class TestaLacos {
+    public static void main(String[] args) {
+        for (int mutiplicador = 1; mutiplicador <= 10; mutiplicador++) {
+            for (int contador = 0; contador <= 10; contador++) {
+                System.out.println(mutiplicador + "X" + contador + "=" + mutiplicador * contador);
+            }
+        }
+    }
+}
