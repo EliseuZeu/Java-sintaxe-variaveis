7d3334a5f8910095bd1cfba9b6c8ff1170bd9f5e
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-15 14:12:26 -0300

    representatando o saldo da conta

--- ZenitBank/src/CriaConta.java
+++ ZenitBank/src/CriaConta.java
@@ -1,5 +1,14 @@
 public class CriaConta {
     public static void main(String[] args) {
-        new Conta();
+        Conta primeirConta = new Conta();
+        primeirConta.saldo = 200;
+        System.out.println(primeirConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 500;
+
+        System.out.println("Primeira conta tem: " + primeirConta.saldo);
+        System.out.println("Segunda conta tem: " + segundaConta.saldo);
+
     }
 }
