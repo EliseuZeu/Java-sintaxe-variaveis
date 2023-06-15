604259409af49d3f1491c2ad32cdfc811a098b83
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-15 18:27:03 -0300

    metodo em boleano em sacar

--- ZenitBank/src/Conta.java
+++ ZenitBank/src/Conta.java
@@ -7,4 +7,13 @@ public class Conta {
     void deposita(double valor) {
         this.saldo += valor;
     }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
