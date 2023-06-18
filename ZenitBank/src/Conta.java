8a8202771a7d3cc814afbc478dbaba784e70317a
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-06-18 14:27:37 -0300

    acrescentando metodo transfere para conta destino

--- ZenitBank/src/Conta.java
+++ ZenitBank/src/Conta.java
@@ -9,11 +9,21 @@ public class Conta {
     }
 
     public boolean saca(double valor) {
+
         if (this.saldo >= valor) {
             this.saldo = this.saldo - valor;
             return true;
         } else {
             return false;
         }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
     }
 }
